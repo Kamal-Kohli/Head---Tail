bfa834292ceca07497f9429591ef2a3be73c5a8f
Kamal Kohli <kamalkohli@Kamals-MacBook-Pro.local>
2024-02-12 21:37:59 +0100

    add API key in news_api/views.py

--- news_api/views.py
+++ news_api/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+import requests
+API_KEY = '2228671f48644d399e8cc37fb14015f6'
 
 # Create your views here.
+
+def home(request):
+    url = f'https://newsapi.org/v2/top-headlines?country=us&apiKey={API_KEY}'
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
+    articles = data['articles']
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'news_api/home.html', context)
